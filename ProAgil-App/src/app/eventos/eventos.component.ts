d601ecb228052b64d007868b53823e23789379c6
Willian Arruda <arrudawillian@hotmail.com>
2019-09-01 03:26:14 -0300

    upload files

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -24,8 +24,10 @@ export class EventosComponent implements OnInit {
   mostrarImg: boolean;
   registerForm: FormGroup;
   bodyDeletarEvento: any;
-
+  file: File;
   _filtroLista: string;
+  currentFileName: string;
+
 
   constructor(
     private eventoService: EventoService
@@ -54,7 +56,7 @@ export class EventosComponent implements OnInit {
     return this.eventoService.getAllEvento().subscribe((_eventos: Evento[]) => {
       this.eventos = _eventos;
       this.eventosFiltrados = this.eventos;
-      console.log('eventos',_eventos);
+      console.log('eventos', _eventos);
     }, error => {
       this.toastr.error(`Erro ao tentar carregar eventos: ${error}`)
       console.log(error);
@@ -78,7 +80,7 @@ export class EventosComponent implements OnInit {
       tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
       local: ['', Validators.required],
       dataEvento: ['', Validators.required],
-      imagemURL: ['', Validators.required],
+      imagemURL: ['',],
       qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
       telefone: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]]
@@ -88,8 +90,10 @@ export class EventosComponent implements OnInit {
   editarEvento(evento: Evento, template: any) {
     this.modoSalvar = 'put';
     this.openModal(template);
-    this.evento = evento;
-    this.registerForm.patchValue(evento);
+    this.evento = Object.assign({}, evento);
+    this.currentFileName = evento.imagemURL.toString();
+    this.evento.imagemURL = '';
+    this.registerForm.patchValue(this.evento);
   }
 
   novoEvento(template: any) {
@@ -97,21 +101,48 @@ export class EventosComponent implements OnInit {
     this.openModal(template);
   }
 
+  onFileChange(event) {
+    //perguntar pq não usa isso?
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      this.file = event.target.files;
+      console.log(this.file);
+    }
+  }
+
+  uploadImagem() {
+    const nomeArquivo = this.evento.imagemURL.split('\\', 3);
+    this.evento.imagemURL = nomeArquivo[2];
+    this.eventoService.postUpload(this.file, nomeArquivo[2]).subscribe(()=>{
+      this.getEventos();
+    });
+  }
+
   salvarAlteracao(template: any) {
     if (this.registerForm.valid) {
       if (this.modoSalvar === 'post') {
         this.evento = Object.assign({}, this.registerForm.value);
+
+        this.uploadImagem();
+
         this.eventoService.postEvento(this.evento).subscribe((novoEvento: Evento) => {
           template.hide();
           this.getEventos();
           this.toastr.success('Evento criado com sucesso');
         }, error => {
           this.toastr.error(`Erro ao criar o evento: ${error}`)
-          console.log("error", error)
         });
       } else {
         this.evento = Object.assign({ id: this.evento.id }, this.registerForm.value);
-        console.log("this.evento", this.evento);
+
+        if (this.evento.imagemURL != '')
+        {
+          this.uploadImagem();
+        } else {
+          this.evento.imagemURL = this.currentFileName;
+        }
+
         this.eventoService.putEvento(this.evento).subscribe((novoEvento: Evento) => {
           template.hide();
           this.getEventos();
@@ -130,7 +161,7 @@ export class EventosComponent implements OnInit {
     this.evento = evento;
     this.bodyDeletarEvento = `Tem certeza que deseja excluir o Evento: ${evento.tema}, Código: ${evento.id}`;
   }
-  
+
   confirmeDelete(template: any) {
     this.eventoService.deleteEvento(this.evento.id).subscribe(
       () => {
